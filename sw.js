4333ad104f891bed8f1e4f1f3469c181be3965c4
KhartW <139468577+KhartW@users.noreply.github.com>
2026-09-05 07:47:27 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = "pf-v2";
+const C = "pf-v3";
 self.addEventListener("install", e => { self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(self.clients.claim()); });
 self.addEventListener("fetch", e => {
